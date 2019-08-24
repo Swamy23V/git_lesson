d76cef10b52b262059cd31966611b22412704410
Swaminathan V <swaminathan.v@refinitiv.com>
2019-08-24 17:43:11 -0400

    added a second function for calculating square

--- src/my_square.py
+++ src/my_square.py
@@ -1,8 +1,12 @@
-def my_sqaure(y):
+def my_square(y):
     """takes a value and returns the squared value.
     
     uses the * operator
     """
     return(y ** 2)
 
+def my_square2(x):
+    return(x * x)
+
 print(my_square(42))
+print(my_square2(42))
